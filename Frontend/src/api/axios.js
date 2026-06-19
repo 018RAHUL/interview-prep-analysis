bd6ff5cf3358d893f1ac44536c247ac925073ef0
Rahul <your-email@gmail.com>
2026-06-19 22:55:30 +0530

    Update production API URL

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "https://interview-prep-analysis-1.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
